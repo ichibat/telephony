c25bb15fd4a35863b2bb67b91921f7ab6594a391
tamotsu ichiba <ichibat@gmail.com>
2020-04-15 23:12:51 +0900

    improved

--- models/patient.js
+++ models/patient.js
@@ -1,1 +1,29 @@
 const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+  karteNo: {
+    type: Number,
+    required: [true, 'カルテ番号を入力してください．'],
+    unique: true,
+    maxlength: [5]
+  }
+  ptFirstName: {
+    type: String,
+    required: [true, '名前を入力してください．'],
+    unique: false,
+    trim: true,
+    maxlength: [24, '名前が長すぎます．']
+  },
+  birthday: {
+    type: Number,
+    required: [true, '生年月日を19660221の形式で入力してください．'],
+    maxlength: [8, '長すぎます．']
+  },
+  disease: {
+    type: String,
+    required: [true, '病名を入力してください．'],
+    unique: false,
+    trim: true,
+    maxlength: [24, '病名が長すぎます．']
+  }
+})
