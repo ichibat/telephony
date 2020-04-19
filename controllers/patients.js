722622917b88cbda18d7f072e949be97c74af6ee
tamotsu ichiba <ichibat@gmail.com>
2020-04-19 23:05:16 +0900

    improved

--- controllers/patients.js
+++ controllers/patients.js
@@ -44,7 +44,7 @@ exports.getPatients = asyncHandler(async(req, res, next) => {
     
     // Pagination
     const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10)　|| 1;
+    const limit = parseInt(req.query.limit, 10)　|| 25;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const total = await Patient.countDocuments();
@@ -73,7 +73,6 @@ exports.getPatients = asyncHandler(async(req, res, next) => {
 
     res.status(200).json({ success: true, count: patients.length, pagination, data: patients });
 
-    console.log(queryStr);
   
   });
 
