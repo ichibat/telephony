ab491e92adb4a9c8dbf8aac00856c07f572d51ef
tamotsu ichiba <ichibat@gmail.com>
2020-04-19 11:54:08 +0900

    after lesson 29

--- controllers/patients.js
+++ controllers/patients.js
@@ -1,74 +1,54 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Patient = require('../models/Patient');
+const asyncHandler = require('../middleware/async');
 
 
 //  @desc   Get all patients
 //  @route  Get /api/v1/patients
 //  @access Public for now
 
-exports.getPatients = async(req, res, next) => {
-  try {
+exports.getPatients = asyncHandler(async(req, res, next) => {
+  
     const patients = await Patient.find();
     res.status(200).json({ success: true, count: patients.length, data: patients });
-  } catch (err) {
-    // res.status(400).json({ success: false});
-    next(err);
-  }
+  
+  });
 
-}
+
 
 //  @desc   Get single patients
 //  @route  Get /api/v1/patients/:id
 //  @access Public for now
 
-exports.getPatient = async (req, res, next) => {
-  try {
+exports.getPatient = asyncHandler(async (req, res, next) => {
+  
     const patient = await Patient.findById(req.params.id);
 
     if(!patient) {
-      // return res.status(400).json({ success: false });
       return next(new ErrorResponse(`idが${req.params.id}の患者さんをみつけることはできませんでした．`,404));
     }
 
     res.status(200).json({ success: true, data: patient });
-  } catch (err) {
-    // res.status(400).json({ success: false });
-    // next(new ErrorResponse(`idが${req.params.id}の患者さんをみつけることはできませんでした．`,404));
-    next(err);
-  }
-}
+  });
+
 
 //  @desc   Create new patient
 //  @route  Post /api/v1/patients
 //  @access Private
 
-exports.createPatient = async (req, res, next) => {
-  try {
+exports.createPatient = asyncHandler(async (req, res, next) => {
+
     const patient = await Patient.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: patient
-    });
-    
-  } catch (err) {
-    // res.status(400).json({
-    //   success: false
-    // });
-    next(err);
-
-  }
-
-
-
-}
+    res.status(201).json({success: true, data: patient});
+  });
 
 //  @desc   Update single patient
 //  @route  PUT /api/v1/patients/:id
 //  @access Private
 
-exports.updatePatient = async (req, res, next) => {
-  try {
+exports.updatePatient = asyncHandler(async (req, res, next) => {
+
     const patient = await Patient.findByIdAndUpdate(req.params.id, req.body, {
       new: true, 
       runValidataors: true
@@ -78,33 +58,19 @@ exports.updatePatient = async (req, res, next) => {
 
     }
     res.status(200).json({ success: true, data: patient });
-  } catch (err) {
-    // res.status(400).json({
-    //   success: false
-    // });
-    next(err);
-
-  }
-  };
+  });
 
 
 //  @desc   Delete single patient
 //  @route  DELETE /api/v1/patients/:id
 //  @access Private
 
-exports.deletePatient = async (req, res, next) => {
-  try {
+exports.deletePatient = asyncHandler(async (req, res, next) => {
     const patient = await Patient.findByIdAndDelete(req.params.id);
     if(!patient) {
       return next(new ErrorResponse(`idが${req.params.id}の患者さんをみつけることはできませんでした．`,404));
 
     }
     res.status(200).json({ success: true, data: {patient} });
-  } catch (err) {
-    // res.status(400).json({
-    //   success: false
-    // });
-    next(err);
-
-  }
-}
+  
+});
