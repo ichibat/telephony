d17f577b289f5e2a6856b98d2d7f494382e6674f
tamotsu ichiba <ichibat@gmail.com>
2020-04-20 12:22:43 +0900

    severity

--- models/Course.js
+++ models/Course.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 
 const CourseSchema = new mongoose.Schema({
+
+  severityAtFirst: {
+    type: String,
+    required: [true, '発症時の重症度を入力してください．'],
+    enum: ['軽症','中等症','重症','不明']
+  },
   currentFluHistory: {
     type: String,
-    required: [true, '病歴を入力してください．不明の場合は，なしと入力してください．'],
+    required: [true, '今回の風邪症状の病歴を入力してください．不明の場合は，なしと入力してください．'],
     unique: false,
     trim: true,
     maxlength: [300, '病歴が長すぎます．']
@@ -14,10 +20,11 @@ const CourseSchema = new mongoose.Schema({
     default: Date.now
   },
   patient: {
-
+    type: mongoose.Schema.ObjectId,
+    ref: 'Patient',
+    required: true
   }
 })
-
 
 
 module.exports = mongoose.model('Course', CourseSchema);
