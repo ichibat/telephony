a445dda90af8a1b3921323b395814a47253135b0
tamotsu ichiba <ichibat@gmail.com>
2020-04-26 09:17:15 +0900

    improved

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 //  @desc   Register user
-//  @route  GET /api/v1/auth/register
+//  @route  POST /api/v1/auth/register
 //  @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -18,6 +18,41 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({　success: true, token　});
+});
+
+
+
+//  @desc   Login user
+//  @route  POST /api/v1/auth/login
+//  @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const {email, password} = req.body;
+
+  // Validate email & password
+ if(!email || !password) {
+   return next(new ErrorResponse('Eメールアドレス，パスワードを入力して下さい．', 400));
+ }
+ // Check for user
+ const user = await User.findOne({ email }).select('+password');
+
+ if(!user) {
+  return next(new ErrorResponse('Eメールアドレス，パスワードが認証できません．', 401));
+ }
+
+ // Check if password matches
+ const isMatch = await user.matchPassword(password);
+
+ if(!isMatch) {
+  return next(new ErrorResponse('Eメールアドレス，パスワードが認証できません．', 401));
+ }
+
+
 
   // Create token
   const token = user.getSignedJwtToken();
