443536124d2df175a21d3ff4e6dad1f6841255dc
tamotsu ichiba <ichibat@gmail.com>
2020-04-30 08:09:06 +0900

    improved

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,7 +54,44 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 
-// Get token from model, create cooke and send response
+
+
+//  @desc   Get current logged in user
+//  @route  POST /api/v1/auth/me
+//  @access Private
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+  });
+
+
+//  @desc   Forgot password
+//  @route  POST /api/v1/auth/forgotpassword
+//  @access Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+    
+if(!user) {
+  return next(new ErrorResponse('このemailアドレスのユーザーはみつかりません．',　404));
+}
+
+// Get reset token
+const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+  
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+  });
+
+  // Get token from model, create cooke and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
@@ -77,16 +114,3 @@ const sendTokenResponse = (user, statusCode, res) => {
       token
     });
 }
-
-//  @desc   Get current logged in user
-//  @route  POST /api/v1/auth/me
-//  @access Private
-
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user
-  });
-  });
